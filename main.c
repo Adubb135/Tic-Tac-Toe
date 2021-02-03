32ffc58a820f6875c3f6a51d4e9966af6202972e
Andrew White <66645240+Adubb135@users.noreply.github.com>
2021-02-02 17:10:31 -0800

    Updated main.c
    Implemented the checkForWin function in here and set up the ternary operator to toggle back-and-forth from Player 1 and Player 2

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Defined functions
+void display_board(void);
 int checkForWin(void);
+
 int player = 1;
+int choice = 0;
 
 int main(void)
 {
   do{
-    
-    
+   display_board();
+   printf("\nPlayer %d, mark a box please: ", player = (player == 2) ? 1 : 2);
+   scanf("%d", &choice);
+   
+   
+       
+   status = checkForWin(); 
   }while (status == 0);
   
   
