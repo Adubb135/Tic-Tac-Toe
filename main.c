621dbd0b8d952402980047bbca88dca69f282ee0
Andrew White <66645240+Adubb135@users.noreply.github.com>
2021-02-03 10:19:56 -0800

    Updated main.c
    Added comment that indicate why we should put the array as a global variable.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Defined functions
+// Declared functions
 void display_board(void);
 int checkForWin(void);
 void mark_board(int a);
@@ -9,6 +9,10 @@ void mark_board(int a);
 int player = 1;
 int choice = 0;
 int status = 0;
+
+/** We should put the char array here so that all functions have access to it besides of 
+ it being in the display_board function and me having to constantly run that function in 
+ other functions. **/
 
 int main(void)
 {
@@ -23,7 +27,7 @@ int main(void)
      // If user chooses a number outside of the bounds, prompt her/him to choose a valid number
      while (choice >= 10 && choice <= 0)
      {
-       printf("Please choose a number between 1 and 9: ");
+       printf("Invalid choice. Please choose a number between 1 and 9: ");
        scanf("%d", &choice);
      }
       
